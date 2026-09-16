968d70e317d528fe081a9e488871aa7dcf42a450
Luz Seren <luz2002seren@gmail.com>
2026-09-16 12:09:16 +0200

    Delete run-nodelink.cmd

--- run-nodelink.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\WINDOWS 10\Desktop\web 2\.nodelink"
-"C:\Program Files\nodejs\node.exe" --dns-result-order=ipv4first dist\src\index.js >> nodelink.log 2>> nodelink-error.log
